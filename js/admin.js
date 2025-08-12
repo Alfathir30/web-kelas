efa840c7b75e36d4f64215b5f8d51a5463944a68
MarvelzId <nandien4409@gmail.com>
2025-08-12 20:15:22 +0700

    update

--- js/admin.js
+++ js/admin.js
@@ -47,7 +47,7 @@ function setupLogin() {
   const password = document.getElementById("password").value
 
   // List password yang boleh akses admin
-  const validPasswords = ["tkj2admin", "admin123", "superpass"]
+  const validPasswords = ["vel", "jnjkdn", "kjsdnkjaj"]
 
   if (validPasswords.includes(password)) {
     loginModal.style.display = "none"
