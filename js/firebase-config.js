a2c10d15d432badc1444fcbc7ae1a8fdb5df0d72
Alfathir30 <aalfathir@gmail.com>
2025-08-12 11:43:36 +0700

    cihuy

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,14 +1,14 @@
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBnWBIil8PKjFXoPRXI4NtgAv6mVNkL89s",
-  authDomain: "tkj2-2db1e.firebaseapp.com",
-  databaseURL: "https://tkj2-2db1e-default-rtdb.firebaseio.com",
-  projectId: "tkj2-2db1e",
-  storageBucket: "tkj2-2db1e.firebasestorage.app",
-  messagingSenderId: "1017107912057",
-  appId: "1:1017107912057:web:15aef0b74d29d3d0bc8ba8",
-  measurementId: "G-29W8RY4XV5"
-};
+    apiKey: "AIzaSyBnWBIil8PKjFXoPRXI4NtgAv6mVNkL89s",
+    authDomain: "tkj2-2db1e.firebaseapp.com",
+    databaseURL: "https://tkj2-2db1e-default-rtdb.firebaseio.com",
+    projectId: "tkj2-2db1e",
+    storageBucket: "tkj2-2db1e.firebasestorage.app",
+    messagingSenderId: "1017107912057",
+    appId: "1:1017107912057:web:0754a2adc6d4b686bc8ba8",
+    measurementId: "G-440VKHG02X"
+  }
 
 // Initialize Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"
